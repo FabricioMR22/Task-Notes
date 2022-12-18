bd4d6e4e4c9117121ad09da9655feff2a98a098a
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-12-17 22:04:07 -0500

    Iniciando las rutas y la conexion a base de datos

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const taskRouter = require('./routes/task.routes')
+app.use(morgan('dev'))
+
+app.use(taskRouter)
+app.listen(4000)
+console.log('server on port 4000')
