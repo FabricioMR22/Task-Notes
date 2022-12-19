1be3f1a92f5b21d08a1f41af192cc4bb883b9840
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-12-18 20:23:14 -0500

    fecth(post) al backend y validando button

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -5,9 +5,11 @@ import {
   CardContent,
   TextField,
   Button,
+  CircularProgress,
 } from "@mui/material";
 
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TaskForm() {
   const [task, setTask] = useState({
@@ -15,9 +17,24 @@ export default function TaskForm() {
     description: "",
   });
 
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(task);
+
+    setLoading(true);
+
+    const res = await fetch("http://localhost:4000/tasks", {
+      method: "POST",
+      body: JSON.stringify(task),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+
+    setLoading(false);
+    navigate("/");
   };
 
   const handleChange = (e) => {
@@ -64,8 +81,17 @@ export default function TaskForm() {
                 inputProps={{ style: { color: "white" } }}
                 InputLabelProps={{ style: { color: "white" } }}
               />
-              <Button variant="contained" color="primary" type="submit">
-                Save
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={!task.title || !task.description}
+              >
+                {loading ? (
+                  <CircularProgress color="inherit" size={24} />
+                ) : (
+                  "Create"
+                )}
               </Button>
             </form>
           </CardContent>
