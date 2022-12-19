237317b9230b2296ef0ebaab2d47d1a244230019
Raul Fabricio Martinez Ramos <u19218420@utp.edu.pe>
2022-12-18 21:54:25 -0500

    fetch get al backend ,componentes react y estilos

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,3 +1,60 @@
+import { useEffect, useState } from "react";
+import { Button, Card, CardContent, Typography } from "@mui/material";
+
 export default function Navbar() {
-  return <div>TaskList Hola</div>;
+  const [tasks, setTasks] = useState([]);
+
+  const loadTasks = async () => {
+    const res = await fetch("http://localhost:4000/tasks");
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>Task List</h1>
+      {tasks.map((task) => (
+        <Card
+          style={{
+            marginBottom: ".7rem",
+            backgroundColor: "#1e272e",
+          }}
+        >
+          <CardContent
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{ color: "white" }}>
+              <Typography>{task.title}</Typography>
+              <Typography>{task.description}</Typography>
+            </div>
+
+            <div>
+              <Button
+                variant="contained"
+                color="inherit"
+                onClick={() => console.log("Edit")}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="contained"
+                color="warning"
+                onClick={() => console.log("Delete")}
+                style={{ marginLeft: ".5rem" }}
+              >
+                Delete
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
 }
